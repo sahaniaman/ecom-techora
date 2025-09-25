dcead795e8c4fb11730d9bfc1c628d775bb8fb19
Aman Sahani <sahaniaman480@gmail.com>
2025-09-26 01:22:08 +0530

    Self overview t9

---
+++ app/api/user/create/route.ts
@@ -0,0 +1,44 @@
+import { NextResponse } from "next/server";
+import dbConnect from "@/lib/mongodb";
+import User, { IUser } from "@/models/User";
+
+export async function POST(req: Request) {
+  try {
+    await dbConnect();
+
+    const body = await req.json();
+    const {
+      id: clerkId,
+      email_addresses,
+      first_name,
+      last_name,
+      image_url,
+    } = body.data;
+
+    // Check if user exists
+    const existing = await User.findOne({ clerkId });
+    if (existing) return NextResponse.json({ ok: true });
+
+    // First user should be Admin
+    const userCount = await User.countDocuments();
+    const role = userCount === 0 ? "SUPER_ADMIN" : "USER";
+
+    const newUser: IUser = await User.create({
+      clerkId,
+      email: email_addresses[0].email_address,
+      profile: {
+        firstName: first_name,
+        lastName: last_name,
+        avatar: image_url,
+      },
+      role,
+    });
+
+    console.log("user created succefully", newUser);
+
+    return NextResponse.json({ ok: true }, { status: 201 });
+  } catch (error) {
+    console.error("Webhook error:", error);
+    return NextResponse.json({ error: "Webhook failed" }, { status: 500 });
+  }
+}
--- app/api/webhooks/clerk/route.ts
+++ app/api/webhooks/clerk/route.ts
@@ -1,45 +1,123 @@
+import type { WebhookEvent } from "@clerk/nextjs/server";
+import { headers } from "next/headers";
 import { NextResponse } from "next/server";
-import  dbConnect  from "@/lib/mongodb";
-import User, { IUser } from "@/models/User";
+import { Webhook } from "svix";
+import dbConnect from "@/lib/mongodb";
+import User from "@/models/User";
 
+export async function POST(request: Request) {
+  const CLERK_WEBHOOK_SECRET = process.env.CLERK_WEBHOOK_SECRET;
 
-export async function POST(req: Request) {
+  if (!CLERK_WEBHOOK_SECRET) {
+    throw new Error(
+      "Please define the CLERK_WEBHOOK_SECRET environment variable inside .env",
+    );
+  }
+
+  const headerPayload = headers();
+  const awaitedHeaderPayload = await headerPayload;
+
+  const svix_id = awaitedHeaderPayload.get("svix-id");
+  const svix_timestamp = awaitedHeaderPayload.get("svix-timestamp");
+  const svix_signature = awaitedHeaderPayload.get("svix-signature");
+
+  if (!svix_id || !svix_timestamp || !svix_signature) {
+    return new Response("Error Occured - No svix header");
+  }
+
+  const payload = await request.json();
+  const body = JSON.stringify(payload);
+  
+  const wh = new Webhook(CLERK_WEBHOOK_SECRET);
+  
+  let evt: WebhookEvent;
+  
   try {
-    await dbConnect();
+    evt = wh.verify(body, {
+      "svix-id": svix_id,
+      "svix-signature": svix_signature,
+      "svix-timestamp": svix_timestamp,
+    }) as WebhookEvent;
+  } catch (err) {
+    console.error("Error Verifying Webhook", err);
+    return new Response("Error Occured - Verifying Webhook", { status: 400 });
+  }
+  
+  const { id } = evt.data;
+  const eventType = evt.type;
+  
+  console.log("CLERK_WEBHOOK_SECRET     ", CLERK_WEBHOOK_SECRET);
+  console.log("payload body     ", body);
+  console.log("evt     ", evt);
+  console.log("id     ", id);
+  console.log("eventType     ", eventType);
+  
+  if (eventType === "user.created") {
+    try {
+      dbConnect();
 
-    const body = await req.json();
-    const {
-      id: clerkId,
-      email_addresses,
-      first_name,
-      last_name,
-      image_url,
-    } = body.data;
-
-    // Check if user exists
-    const existing = await User.findOne({ clerkId });
-    if (existing) return NextResponse.json({ ok: true });
-
-    // First user should be Admin
+          // First user should be Admin
     const userCount = await User.countDocuments();
     const role = userCount === 0 ? "SUPER_ADMIN" : "USER";
 
-    const newUser: IUser = await User.create({
-      clerkId,
-      email: email_addresses[0].email_address,
-      profile: {
-      firstName: first_name,
-      lastName: last_name,
-      avatar: image_url
-    },
-      role,
-    });
+      const {
+        id,
+        first_name,
+        last_name,
+        email_addresses,
+        primary_email_address_id,
+        phone_numbers,
+        primary_phone_number_id,
+        image_url,
+        has_image,
+      } = evt.data;
 
-    console.log("user created succefully", newUser)
+      const clerkId = id;
 
-    return NextResponse.json({ ok: true }, { status: 201 });
-  } catch (error) {
-    console.error("Webhook error:", error);
-    return NextResponse.json({ error: "Webhook failed" }, { status: 500 });
+      const primaryEmail = email_addresses.find(
+        (email) => email.id === primary_email_address_id,
+      );
+
+      const primaryPhoneNumber = phone_numbers.find(
+        (phoneNumber) => phoneNumber.id === primary_phone_number_id
+      )
+
+      let avatar: string = "";
+
+      if (has_image) {
+        avatar = image_url;
+      }
+
+      console.log("primaryEmail   ", primaryEmail);
+
+      if (!primaryEmail) {
+        return new Response("No Primary Email was Found.", { status: 404 });
+      }
+
+      // Recommended with basic profile
+      const newUser = await User.create({
+        clerkId: clerkId,
+        email: primaryEmail.email_address,
+        phone: primaryPhoneNumber?.phone_number,
+        role: role,
+        profile: {
+          firstName: first_name,
+          lastName: last_name,
+          avatar: avatar,
+        },
+      });
+
+      return new Response("User Created in Database from webhook", newUser)
+
+    } catch (err) {
+      console.error("Error creating user   ", err)
+      return new Response("Error Occured", {status: 400})
+    }
   }
+
+  return new Response("Webhook Recieved Successfully")
+}
+
+export async function GET() {
+  return NextResponse.json({ message: "Webhook endpoint" });
 }
